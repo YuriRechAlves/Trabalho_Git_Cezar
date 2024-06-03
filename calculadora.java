741cd292d761d56fff5e0d14ac80a6895eadce57
YuriRechAlves <yurirech2003@gmail.com>
2024-06-03 20:54:59 -0300

    Criado a operacao de soma

---
+++ calculadora.java
@@ -0,0 +1,9 @@
+public class SomaNumeros {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 5;
+        int soma = num1 + num2;
+
+        System.out.println("A soma de " + num1 + " + " + num2 + " é igual a " + soma);
+    }
+}
