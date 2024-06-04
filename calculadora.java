08cc5929cf36a196cc1fdadaa649a0d10a03d3fd
YuriRechAlves <yurirech2003@gmail.com>
2024-06-03 21:11:31 -0300

    Alterado o titulo da classe para calculadora e inserido o metodo subtrair

--- calculadora.java
+++ calculadora.java
@@ -1,9 +1,10 @@
-public class SomaNumeros {
+public class calculadora {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 5;
-        int soma = num1 + num2;
-
-        System.out.println("A soma de " + num1 + " + " + num2 + " é igual a " + soma);
-    }
+        double num1 = 5;
+        double num2 = 5;
+        double soma = num1 + num2;
+	double subtracao = num1 - num2;
+        System.out.println("A soma de " +num1+ " + " +num2+ " é igual a "+soma);
+	System.out.println("A subtracao de " +num1+ " - " +num2+ "e igual a "+subtracao);	
+}
 }
